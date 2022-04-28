7c9b2addde08918cfc0216d456832fd6dbdec292
Richard Vu <richardvu.work@gmail.com>
2022-04-28 16:06:15 +0700

    Update config

--- Backend/config-service/src/main/java/com/richard/application/ConfigServiceApplication.java
+++ Backend/config-service/src/main/java/com/richard/application/ConfigServiceApplication.java
@@ -2,6 +2,7 @@ package com.richard.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
